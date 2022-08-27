d439a84b53894a10cd6b6cf89570bfebb4db3275
Patrick Kerwood <patrick@kerwood.dk>
2022-08-27 22:20:14 +0200

    feat: added support for json dashboards
    Signed-off-by: Patrick Kerwood <patrick@kerwood.dk>

--- apis/dashboard/v1alpha1/zz_generated.managed.go
+++ apis/dashboard/v1alpha1/zz_generated.managed.go
@@ -84,3 +84,69 @@ func (mg *Dashboard) SetPublishConnectionDetailsTo(r *xpv1.PublishConnectionDeta
 func (mg *Dashboard) SetWriteConnectionSecretToReference(r *xpv1.SecretReference) {
 	mg.Spec.WriteConnectionSecretToReference = r
 }
+
+// GetCondition of this DashboardJSON.
+func (mg *DashboardJSON) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
+	return mg.Status.GetCondition(ct)
+}
+
+// GetDeletionPolicy of this DashboardJSON.
+func (mg *DashboardJSON) GetDeletionPolicy() xpv1.DeletionPolicy {
+	return mg.Spec.DeletionPolicy
+}
+
+// GetProviderConfigReference of this DashboardJSON.
+func (mg *DashboardJSON) GetProviderConfigReference() *xpv1.Reference {
+	return mg.Spec.ProviderConfigReference
+}
+
+/*
+GetProviderReference of this DashboardJSON.
+Deprecated: Use GetProviderConfigReference.
+*/
+func (mg *DashboardJSON) GetProviderReference() *xpv1.Reference {
+	return mg.Spec.ProviderReference
+}
+
+// GetPublishConnectionDetailsTo of this DashboardJSON.
+func (mg *DashboardJSON) GetPublishConnectionDetailsTo() *xpv1.PublishConnectionDetailsTo {
+	return mg.Spec.PublishConnectionDetailsTo
+}
+
+// GetWriteConnectionSecretToReference of this DashboardJSON.
+func (mg *DashboardJSON) GetWriteConnectionSecretToReference() *xpv1.SecretReference {
+	return mg.Spec.WriteConnectionSecretToReference
+}
+
+// SetConditions of this DashboardJSON.
+func (mg *DashboardJSON) SetConditions(c ...xpv1.Condition) {
+	mg.Status.SetConditions(c...)
+}
+
+// SetDeletionPolicy of this DashboardJSON.
+func (mg *DashboardJSON) SetDeletionPolicy(r xpv1.DeletionPolicy) {
+	mg.Spec.DeletionPolicy = r
+}
+
+// SetProviderConfigReference of this DashboardJSON.
+func (mg *DashboardJSON) SetProviderConfigReference(r *xpv1.Reference) {
+	mg.Spec.ProviderConfigReference = r
+}
+
+/*
+SetProviderReference of this DashboardJSON.
+Deprecated: Use SetProviderConfigReference.
+*/
+func (mg *DashboardJSON) SetProviderReference(r *xpv1.Reference) {
+	mg.Spec.ProviderReference = r
+}
+
+// SetPublishConnectionDetailsTo of this DashboardJSON.
+func (mg *DashboardJSON) SetPublishConnectionDetailsTo(r *xpv1.PublishConnectionDetailsTo) {
+	mg.Spec.PublishConnectionDetailsTo = r
+}
+
+// SetWriteConnectionSecretToReference of this DashboardJSON.
+func (mg *DashboardJSON) SetWriteConnectionSecretToReference(r *xpv1.SecretReference) {
+	mg.Spec.WriteConnectionSecretToReference = r
+}
--- apis/dashboard/v1alpha1/zz_generated.managedlist.go
+++ apis/dashboard/v1alpha1/zz_generated.managedlist.go
@@ -19,6 +19,15 @@ package v1alpha1
 
 import resource "github.com/crossplane/crossplane-runtime/pkg/resource"
 
+// GetItems of this DashboardJSONList.
+func (l *DashboardJSONList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
 // GetItems of this DashboardList.
 func (l *DashboardList) GetItems() []resource.Managed {
 	items := make([]resource.Managed, len(l.Items))
